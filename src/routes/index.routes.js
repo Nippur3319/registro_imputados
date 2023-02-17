7eb13c0700ef2072b3b12d18ad9cad6a4a3c0fdc
Nippur3319 <73201004+Nippur3319@users.noreply.github.com>
2023-02-16 21:14:10 -0300

    finalizado update 1:52

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -6,23 +6,39 @@ const router = Router();
 router.get("/", async (req, res) => {
   const tasks = await Task.find().lean();
   //console.log(tasks);
-  res.render("index",{tasks:tasks});
+  res.render("index", { tasks: tasks });
 });
 
 router.post("/tasks/add", async (req, res) => {
-  const task = Task(req.body);
-
-  await task.save();
-
-  res.redirect("/");
+  try {
+    const task = Task(req.body);
+    await task.save();
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get("/about", (req, res) => {
   res.render("about");
 });
 
-router.get("/edit", (req, res) => {
-  res.render("edit");
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id).lean();
+
+    res.render("edit", { task: task });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await Task.findByIdAndUpdate(id, req.body);
+
+  res.redirect("/");
 });
 
 export default router;
